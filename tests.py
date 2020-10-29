e55b0ec53a5cb0dadf3a3a498b45f1312255ba14
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2020-10-29 13:22:55 -0500

    test file

---
+++ tests.py
@@ -0,0 +1,12 @@
+import datetime
+import date_methods
+
+## greates a list of datetime objects for the specified range. To print them formatted us strformat
+start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
+end = datetime.datetime.strptime("2020-01-01", "%Y-%m-%d")
+dates_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+# Test function below 
+for date in dates_generated:
+    fullDate = date_methods.fullDate(date)
+    print(fullDate)
