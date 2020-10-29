d9ca0ee07c394b09d4914ee6d50d2a4738d50746
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2020-10-29 13:21:22 -0500

    reordered function calls and left comments

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import date_methods
 
 filename = "tabels.csv"
 
-# feilds = ["dateId","dateType","fullDate","day","dayName","calendarDayOfWeek","noncalendarDayOfWeek","dayOfQuarter","dayOfYear","monthDay","monthNameDay","monthDayYear","monthNameDayYear","dayYear","yearQuarterMonthWeekDay","dayFrom1900","calendarWeekOfMonth","noncalendarWeekOfMonth","calendarWeekOfQuarter","noncalendarWeekOfQuarter","calendarWeekOfYear","noncalendarWeekOfYear","yearQuarterMonthWeek","month","monthName","monthOfQuarter","monthYear","monthNameYear","yearQuarterMonth","termCode","bannerTermCode","termFrom1900","quarter","quarterName","quarterYear","quarterNameYear","yearQuarter","year","quarterMonthWeekDay","quarterMonthWeek","quarterMonth","dayOfProcess","monthOfProcess","monthNameOfProcess","monthWeekOfProcess","monthDayOfProcess","monthWeekDayOfProcess","isLeapYear","processYear","startDateOfProcess"]
-feilds = ["dateId","dateType", "fullDate", "day", "dayName" ]
+feilds = ["dateId","dateType","fullDate","day","dayName","calendarDayOfWeek","noncalendarDayOfWeek","dayOfQuarter","dayOfYear","monthDay","monthNameDay","monthDayYear","monthNameDayYear","dayYear","yearQuarterMonthWeekDay","dayFrom1900","calendarWeekOfMonth","noncalendarWeekOfMonth","calendarWeekOfQuarter","noncalendarWeekOfQuarter","calendarWeekOfYear","noncalendarWeekOfYear","yearQuarterMonthWeek","month","monthName","monthOfQuarter","monthYear","monthNameYear","yearQuarterMonth","termCode","bannerTermCode","termFrom1900","quarter","quarterName","quarterYear","quarterNameYear","yearQuarter","year","quarterMonthWeekDay","quarterMonthWeek","quarterMonth","dayOfProcess","monthOfProcess","monthNameOfProcess","monthWeekOfProcess","monthDayOfProcess","monthWeekDayOfProcess","isLeapYear","processYear","startDateOfProcess"]
+
 
 
 ## greates a list of datetime objects for the specified range. To print them formatted us strformat
@@ -20,16 +20,38 @@ for date in dates_generated:
     # array that we will append to array rows in line 19
     sub_rows=[]
 
+    # date Id
     dateId = date_methods.dateId(date)
     sub_rows.append(dateId)
 
+    # dateType
     dateType = date_methods.dateType(date)
     sub_rows.append(dateType)
 
+    # fullDate
     fullDate = date_methods.fullDate(date)
     sub_rows.append(fullDate)
 
-    day = date_methods.
+    # day
+    day = date_methods.day(date)
+    sub_rows.append(day)
+
+    # dayName
+    dayName = date_methods.dayName(date)
+    sub_rows.append(dayName)
+
+    # calendarDayOfWeek 
+    calDayOfWeek = date_methods.calDayOfWeek(date)
+    sub_rows.append(calDayOfWeek)
+
+    # noncalendarDayOfWeek" 
+    nonCalDayOfWeek = date_methods.nonCalDayOfWeek(date)
+    sub_rows.append(nonCalDayOfWeek)
+
+    # dayOfQuarter
+    dayOfQuarter = date_methods.dayOfQuarter(date)
+    sub_rows.append(dayOfQuarter)
+
 
     #append to rows in row 19    
     rows.append(sub_rows)
@@ -40,6 +62,3 @@ with open(filename, 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(feilds)
     writer.writerows(rows)
-
-
-
