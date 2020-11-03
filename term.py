09b8fed3c34c294dce887582acbce90b599fe3d6
notjoemartinez <notjoemartinez@gmail.com>
2020-11-03 16:50:06 -0600

    cleaned uyp and commented term.py

--- term.py
+++ term.py
@@ -5,41 +5,33 @@ import datetime
 # has a value of 3, 8/21/1900 has a value of 4, 1/1/1901 has a value of 5, and the cycle continues. This is at least roughly 
 # aligned with our Spring (1/1-5/10), Summer I (5/10-6/30), Summer II (7/1-8/20), and Fall (8/20-12/31) terms
 
-# class Term():
 
-#     termDict = {
-#         1 : "Jan, 1",
-#         2 : "May, 11",
-#         3 : "July, 1"
-#     }
-
-
-#     def __init__ (self, date):
-#         self.date = date
-#         self.first = ""
-
-
+# this thing creates a list in the range of datetime objects starting from 1900 to whatever date you supplied it 
+# it then checks eacho of those date time objects to see if they match up with one of the 4 term dates
 def which_term(date):
-    # starting at jan 1 1900 make a counter that moves up every 
-    count = 0
 
+    # starting at jan 1 1900 make a list of datetime objects up until date 
     start = datetime.datetime(1900,1,1)
     end = date
-
     dates_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 
+    # list of "term enders"
     term_strs = ["01-01","05-11","07-01","08-21"]
+
+    # Iteration counter 
     i = 0
+    # Term counter
     term = 0
-    # if any("abc" in s for s in some_list):
-    # while(datetime.datetime(1901,1,1) > dates_generated[i]):
+
     while(date>dates_generated[i]):
+
+        #  the d_index formating is neccisary to check against out term_strs which triggers the counter
         d_index = dates_generated[i].strftime("%m-%d") 
-        # print(d_index)
+
         if any(d_index in s for s in term_strs):
             term +=1
-            print(term,d_index)
-        # print(i)
+            print(term,dates_generated[i].strftime("%Y-%m-%d"))
+
         if(i+1 >= len(dates_generated)):
             break
         i+=1
@@ -48,10 +40,9 @@ def which_term(date):
         
     
     pass
-    # return "{}, {}".format(len(dates_generated),type(dates_generated[23]))
 
         
 
-
+# TODO: Somehow turn the string back into a datetime object and determin what term the given term number is based on the datetime object
     
-print(which_term(datetime.datetime(1901,1,1)))
+print(which_term(datetime.datetime(2100,1,1)))
