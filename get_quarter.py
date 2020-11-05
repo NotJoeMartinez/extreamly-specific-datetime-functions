47c75b7af37defd3b1aa51fb80d47ad2947c3428
notjoemartinez <notjoemartinez@gmail.com>
2020-11-05 16:46:12 -0600

    refacord dayOfQuarter, added ending test case that checks all three methods

--- get_quarter.py
+++ get_quarter.py
@@ -16,17 +16,17 @@ class Quarter:
         
     def dayOfQuarter(self):
 
-        # q1, q2, q3, q4 = self.q1, self.q2, self.q3, self.q4
+        q1, q2, q3, q4 = self.q1, self.q2, self.q3, self.q4
+        date = self.date
 
-        # date = self.date
-        if(self.q2 > self.date >= self.q1):
+        if(q2 > date >= q1):
             # check to see if date is equal to q1
-            if (self.date == self.q1):
+            if (date == q1):
                 return 1
             # doq is day of quarter
             else:
                 # subtract date by the q1 date time object
-                doq = self.date - self.q1
+                doq = date - q1
                 # get the "days" from that object
                 doq = doq.days
                 # turn it into an int
@@ -34,32 +34,32 @@ class Quarter:
                 # add it one to it 
                 return doq+1
 
-        elif(self.q3 > self.date >= self.q2):
-            if (self.date == self.q2):
+        elif(q3 > date >= q2):
+            if (date == q2):
                 return 1
             else:
                 # subtract date by the q1 date time object
-                doq = self.date - q2
+                doq = date - q2
                 doq = doq.days
                 int(doq)
                 return doq + 1
 
-        elif(self.q4 > self.date >= self.q3):
-            if(self.date == self.q3):
+        elif(q4 > date >= q3):
+            if(date == q3):
                 return 1
             else: 
                 # subtract date by the q1 date time object
-                doq = self.date - q3
+                doq = date - q3
                 doq = doq.days
                 int(doq)
                 return doq + 1
 
-        elif(self.date >= q4):
-            if(self.date == q4):
+        elif(date >= q4):
+            if(date == q4):
                 return(1)
             else:
                 # subtract date by the q1 date time object
-                doq = self.date - q4
+                doq = date - q4
                 doq = doq.days
                 int(doq)
                 return doq + 1
@@ -117,6 +117,8 @@ class Quarter:
 for i in range(1,13):
     obj = Quarter(datetime.datetime(1994,i,i))
 
-    print(obj.quarter())
+    print("The quarter is {}".format(obj.quarter()))
+    print("The month of the quarter is {}".format(obj.monthOfQuarter()))
+    print("The day of the quarter is {}".format(obj.dayOfQuarter()))
     
 
